781abc9137c461d28c405cdd7b658e4ed9edd9d8
Josef Jezek <josef@jezek.co.cz>
2014-01-09 02:33:49 +0100

    First pages commit

---
+++ rocker-buttons/bootswatch/copy.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+VERSION=3.0.3
+
+cp $1/amelia/bootstrap.min.css $VERSION/amelia
+cp $1/cerulean/bootstrap.min.css $VERSION/cerulean
+cp $1/cosmo/bootstrap.min.css $VERSION/cosmo
+cp $1/cyborg/bootstrap.min.css $VERSION/cyborg
+cp $1/custom/bootstrap.min.css $VERSION/default
+cp $1/flatly/bootstrap.min.css $VERSION/flatly
+cp $1/journal/bootstrap.min.css $VERSION/journal
+cp $1/readable/bootstrap.min.css $VERSION/readable
+cp $1/simplex/bootstrap.min.css $VERSION/simplex
+cp $1/slate/bootstrap.min.css $VERSION/slate
+cp $1/spacelab/bootstrap.min.css $VERSION/spacelab
+cp $1/united/bootstrap.min.css $VERSION/united
+cp $1/yeti/bootstrap.min.css $VERSION/yeti
---
+++ rocker-buttons/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  var path_theme = "/bootswatch/3.0.3/";
+
+  $("#theme").attr("href", path_theme + "/default/bootstrap.min.css");
+  $("body").attr("class", theme);
+
+  $("#navbar-main .themes li").click(function() {
+    var theme = $(this).attr("data-theme");
+
+    if ($("body").hasClass(theme)) return;
+
+    $("#navbar-main .themes li").removeClass("active");
+    $(this).addClass("active");
+
+    $("#theme").attr("href", path_theme + theme + "/bootstrap.min.css");
+    $("body").attr("class", theme);
+  });
+
+});
